0fbdd9eb9d8b4198faf5c37a3e5ac019c22fb3c6
Hemant Verma <vermahemant837@gmail.com>
2023-11-03 12:10:07 +0530

    Image Section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import HeroSection from "../HeroSection/HeroSection";
 import MeetSection from "../MeetSection/MeetSection";
+import ImageSection from "../ImageSection/ImageSection";
 
 const HomePage = () => {
   return (
     <Fragment>
       <HeroSection />
       <MeetSection />
+      <ImageSection />
     </Fragment>
   );
 };
---
+++ src/components/ImageSection/ImageSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image1 from "../../assets/images/imageSection/image1.jpg";
+import Image2 from "../../assets/images/imageSection/image2.jpg";
+
+const ImageSection = () => {
+  return (
+    <div className="h-96 flex">
+      <div className="w-1/2 mr-2">
+        <img
+          src={Image1}
+          alt="Image 1"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-1/2 ml-2">
+        <img
+          src={Image2}
+          alt="Image 2"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSection;
